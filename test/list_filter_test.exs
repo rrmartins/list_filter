2a6a67694777ad881f305dea283b7a736cf19fba
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-08 23:50:57 +0000

    add new test

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -2,6 +2,13 @@ defmodule ListFilterTest do
   use ExUnit.Case
 
   describe "call/1" do
-    
+    test "returns 4 when send 4 numbers odd" do
+      size = ["3", "45", "21", "rodrigo", "22"]
+             |> ListFilter.call
+
+      expected_size = 3
+
+      assert expected_size == size
+    end
   end
 end
